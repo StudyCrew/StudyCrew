5d8a02a576a04735bf5268ec3b40a3b1c44a8ebc
scudderkct <kyle@kylescudder.co.uk>
2023-12-06 23:36:04 +0000

    fix(lint): revert linting enforcement during builds

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
-    ignoreDuringBuilds: false,
+    ignoreDuringBuilds: true,
   },
   images: {
     remotePatterns: [
