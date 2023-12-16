003534c6cdcd44c89d96c60e8e5a5c65b748464a
Kyle Scudder <74150974+kylescudder@users.noreply.github.com>
2023-12-16 22:38:58 +0000

    feat(homepage): #14 added homepage to main project
    * feat(homepage): #14 added homepage to main project
    * fix(homepage): #14 removed dropdown from project
    Does not need to be here right now, will re-add later
    * fix(homepage): #14 rewrote Button comp to remove prop-type usage
    Not needed in TypeScript
    * fix(homepage): #14 added default value to carousel useRef
    * fix(homepage): #14 adjusted add/remove of display class
    * feat(homepage): #14 Join Us button now links to form
    * fix(homepage): #14 removed darkmode-toggle from project
    Does not need to be here right now, will re-add later

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,102 @@
+import './Navbar.css'
+
+import React from 'react'
+
+interface NavbarProps {
+	activePage: string
+	setActivePage: React.Dispatch<React.SetStateAction<string>>
+	missionRef: React.MutableRefObject<null>
+	featuresRef: React.MutableRefObject<null>
+	projectRef: React.MutableRefObject<null>
+	teamRef: React.MutableRefObject<null>
+	signupRef: React.MutableRefObject<null>
+	faqRef: React.MutableRefObject<null>
+	setIsMenuOpen: React.Dispatch<React.SetStateAction<boolean>> // Add this line
+}
+
+const Navbar = ({
+	activePage,
+	setActivePage,
+	missionRef,
+	featuresRef,
+	projectRef,
+	teamRef,
+	signupRef,
+	faqRef,
+	setIsMenuOpen // Add this line
+}: NavbarProps): JSX.Element => {
+	// Helper function to determine if a link is active
+	const isActive = (name: string): string => activePage === name ? 'activeNavLink' : ''
+
+	// Scrolls smoothly to the provided reference
+	const scrollToRef = (ref: React.MutableRefObject<null>): void => {
+		const refContainerCurrent = ref.current as HTMLDivElement | null
+		if (refContainerCurrent) {
+			const y = refContainerCurrent.getBoundingClientRect().top + window.scrollY + -60
+			window.scrollTo({ top: y, behavior: 'smooth' })
+		}
+	}
+
+	// Handles the navigation link click based on the section's name
+	const handleNavLinkClick = (name: string): void => {
+		setIsMenuOpen(false)
+
+		let ref
+		switch (name) {
+			case 'mission':
+				ref = missionRef
+				break
+			case 'features':
+				ref = featuresRef
+				break
+			case 'project':
+				ref = projectRef
+				break
+			case 'team':
+				ref = teamRef
+				break
+			case 'signup':
+				ref = signupRef
+				break
+			case 'faq':
+				ref = faqRef
+				break
+			default:
+				return
+		}
+		scrollToRef(ref)
+	}
+
+	return (
+		<nav>
+			{/* Navigation links for different sections */}
+			{/* When clicked, the view scrolls to the respective section */}
+			<div className={isActive('mission')} onClick={() => {
+				handleNavLinkClick('mission')
+				setActivePage('mission')
+			}}>Mission</div>
+			<div className={isActive('features')} onClick={() => {
+				handleNavLinkClick('features')
+				setActivePage('features')
+			}}>Features</div>
+			<div className={isActive('project')} onClick={() => {
+				handleNavLinkClick('project')
+				setActivePage('project')
+			}}>Project</div>
+			<div className={isActive('signup')} onClick={() => {
+				handleNavLinkClick('signup')
+				setActivePage('signup')
+			}}>Sign Up</div>
+			<div className={isActive('team')} onClick={() => {
+				handleNavLinkClick('team')
+				setActivePage('team')
+			}}>Team</div>
+			<div className={isActive('faq')} onClick={() => {
+				handleNavLinkClick('faq')
+				setActivePage('faq')
+			}}>FAQs</div>
+		</nav>
+	)
+}
+
+export default Navbar
