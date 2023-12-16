727420991ed1f874ab8f4b45fa4286e4f3333d71
Kyle Scudder <kyle@kylescudder.co.uk>
2023-12-16 19:44:33 +0000

    fix(homepage): #14 rewrote Button comp to remove prop-type usage
    Not needed in TypeScript

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,36 +1,21 @@
-// Button.js
-
-// Importing necessary libraries and styles
 import React from 'react'
-import PropTypes from 'prop-types'
-import './Button.css' // Styling for the Button component
+import './Button.css'
 
-/**
- * Button Component
- *
- * This component renders a button with customizable properties.
- * It supports different variants and sizes to accommodate various use cases.
- *
- * @param {Object} props - The properties object.
- * @param {React.ReactNode} props.children - The content to be displayed inside the button.
- * @param {string} [props.variant='primary'] - The variant of the button, e.g., 'outline' for an outlined
- * button.
- * @param {string} [props.size] - The size of the button, e.g., 'big' for a larger button.
- * @param {Function} [props.onClick] - The callback to be invoked when the button is clicked.
- *
- * @returns {React.Element} The rendered Button component.
- */
-const Button = ({ children, variant, size, onClick }): React.Element => {
-	// Base class for all buttons
+interface ButtonProps {
+	children: React.ReactNode
+	variant?: 'outline' | 'primary'
+	size?: 'big' | 'small' | 'big-outline'
+	onClick?: () => void
+}
+
+const Button: React.FC<ButtonProps> = ({ children, variant = 'primary', size, onClick }) => {
 	let className = 'button'
 
-	// Modify class based on the variant and size props to apply different styles
 	if (variant === 'outline') className += ' button-outline'
 	if (size === 'big') className += ' button-big'
 	if (size === 'small') className += ' button-small'
 	if (size === 'big-outline') className += ' button-big-outline'
 
-	// Render the button with the computed className and provided props
 	return (
 		<button className={className} onClick={onClick}>
 			{children}
@@ -38,19 +23,4 @@ const Button = ({ children, variant, size, onClick }): React.Element => {
 	)
 }
 
-// Define the types of props for better documentation and validation
-Button.propTypes = {
-	children: PropTypes.node.isRequired, // Content of the button, can be text or another component
-	variant: PropTypes.string, // Variant of the button, e.g., 'outline'
-	size: PropTypes.string, // Size of the button, e.g., 'big'
-	onClick: PropTypes.func // Callback for click event
-}
-
-// Define default values for optional props
-Button.defaultProps = {
-	variant: 'primary', // Default to primary button if no variant is provided
-	onClick: () => {} // Default to no-op function if no onClick is provided
-}
-
-// Export the Button component for use in other files
 export default Button
