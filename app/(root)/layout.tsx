5e9c3232bdf1c56dff4e8d18a04cc27ed28487fe
Kyle Scudder <kyle@kylescudder.co.uk>
2023-12-17 15:43:06 +0000

    refactor(metadata): #24 metadata split into smaller files
    Added some quick POC to auth pages

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -7,6 +7,10 @@ import Head from 'next/head'
 import Link from 'next/link'
 import '../globals.css'
 import '../../styles/landing.css'
+import icons from '@/lib/metadata/icons'
+import openGraph from '@/lib/metadata/openGraph'
+import robots from '@/lib/metadata/robot'
+import twitter from '@/lib/metadata/twitter'
 
 export const viewport: Viewport = {
 	themeColor: '#3A86FF'
@@ -18,40 +22,7 @@ export const metadata: Metadata = {
     'Making education more accessible, collaborative, and engaging.',
 	applicationName: 'StudyCrew',
 	manifest: '/manifest.json',
-	icons: [
-		{
-			url: '/assets/maskable_icon_x48',
-			type: 'image/png',
-			sizes: '48x48'
-		},
-		{
-			url: '/assets/maskable_icon_x72',
-			type: 'image/png',
-			sizes: '72x72'
-		},
-		{
-			url: '/assets/maskable_icon_x96.png',
-			type: 'image/png',
-			sizes: '96x96'
-		},
-		{
-			url: '/assets/maskable_icon_x128.png',
-			type: 'image/png',
-			sizes: '128x128'
-		},
-		{
-			url: '/assets/maskable_icon_x192.png',
-			type: 'image/png',
-			sizes: '192x192',
-			rel: 'apple-touch-icon'
-		},
-		{
-			url: '/assets/maskable_icon_x384.png',
-			type: 'image/png',
-			sizes: '384x384',
-			rel: 'apple-touch-icon'
-		}
-	],
+	icons,
 	appleWebApp: {
 		capable: true,
 		statusBarStyle: 'default',
@@ -60,23 +31,9 @@ export const metadata: Metadata = {
 	formatDetection: {
 		telephone: false
 	},
-	openGraph: {
-		type: 'website',
-		siteName: 'StudyCrew',
-		title: {
-			default: 'StudyCrew',
-			template: '% - PWA App'
-		},
-		description: 'Making education more accessible, collaborative, and engaging.'
-	},
-	twitter: {
-		card: 'summary',
-		title: {
-			default: 'StudyCrew',
-			template: '% - PWA App'
-		},
-		description: 'Making education more accessible, collaborative, and engaging.'
-	}
+	openGraph,
+	robots,
+	twitter
 }
 
 export default async function RootLayout ({
---
+++ lib/metadata/twitter.ts
@@ -0,0 +1,13 @@
+import { type Twitter } from 'next/dist/lib/metadata/types/twitter-types'
+
+const twitter: Twitter = {
+	card: 'summary',
+	title: {
+		default: 'StudyCrew',
+		template: '% - PWA App'
+	},
+	description:
+      'Making education more accessible, collaborative, and engaging.'
+}
+
+export default twitter
