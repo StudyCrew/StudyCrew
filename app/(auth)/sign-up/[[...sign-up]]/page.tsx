b3b6e33c59ec787197687ebcd76c30d8af5441f8
Kyle Scudder <74150974+kylescudder@users.noreply.github.com>
2023-12-21 14:40:35 +0000

    refactor(metadata): #24 metadata split into smaller files (#45)
    Added some quick POC to auth pages

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import { SignUp } from '@clerk/nextjs'
+import { type Metadata } from 'next'
+
+export const metadata: Metadata = {
+	title: 'Sign Up | StudyCrew',
+	description:
+	'Sign up now to start your journey to a better education.'
+}
 
 export default function Page (): JSX.Element {
 	return <SignUp />
