93906e9a17c2b329f36870cb9523d240803d5532
Kyle Scudder <74150974+kylescudder@users.noreply.github.com>
2024-01-01 15:34:54 +0000

    Merge 77-github-action-playwright (#126)
    * fix(secret): #77 updated environment variables
    
    * feat(test): #77 added test script to package.json
    
    * fix(config): #77 updated package.json name to fit casing convention
    
    * feat(test): #77 added github action step to run playwright tests
    
    * fix(test): #77 bump wait-for-vercel-preview version from 1.2.0 to 1.3.1
    
    * fix(test): #77 fix typo in PLAYWRIGHT_TEST_BASE_URL variable value
    
    * fix(test): #77 added missing v from version number

--- tests/homepage/homepage.spec.ts
+++ tests/homepage/homepage.spec.ts
@@ -1,14 +1,15 @@
 import { test, expect } from '@playwright/test'
+import { Page } from '@playwright/test'
 
-test('has title', async ({ page }) => {
-	await page.goto(process.env.BASE_URL ?? 'http://localhost:3000')
+test("has title", async ({ page }: { page: Page }) => {
+  await page.goto("/");
 
-	// Expect a title "to contain" a substring.
-	await expect(page).toHaveTitle(/StudyCrew/)
-})
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/StudyCrew/);
+});
 
-test('has stages', async ({ page }) => {
-	await page.goto(process.env.BASE_URL ?? 'http://localhost:3000')
+test('has stages', async ({ page }: { page: Page }) => {
+	await page.goto('/')
 
 	await page
 		.locator(
