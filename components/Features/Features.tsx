67d8618b33f3472fe4de9fef7e59c0aa394d991d
Cris Mihalache <f3rno64@gmail.com>
2024-01-09 21:36:21 +0100

    refactor: Features component, use classnames, extract + type stages, etc

--- components/Features/Features.tsx
+++ components/Features/Features.tsx
@@ -1,57 +1,47 @@
-import React, { useRef, useState, useEffect } from 'react' // Removed useCallback
-import './Features.css'
-import stages from '../../lib/data/featureData'
 import Image from 'next/image'
+import classNames from 'classnames'
+import React, { useMemo, useState, useCallback } from 'react'
 
-function Features (): JSX.Element {
-	const owlCarousel = useRef<HTMLDivElement | null>(null)
-	const cardsContainer = useRef<HTMLDivElement | null>(null)
-	const [cardWidth, setCardWidth] = useState(0)
-	const [currentStage, setCurrentStage] = useState('stage-1')
+import { type FeaturesStage, FeaturesStageID } from './types'
+import {
+	FEATURES_STAGES,
+	FEATURES_STAGE_ONE,
+	FEATURES_STAGE_TWO,
+	FEATURES_STAGE_THREE
+} from './const'
 
-	// Handle clicks on stages for larger screens
-	const accordian = (id: string): void => {
-		setCurrentStage(id)
-		setCardWidth(0)
+import './Features.css'
 
-		if (owlCarousel.current) {
-			const ele = owlCarousel.current.children
-			console.log(ele)
-			switch (id) {
-				case 'stage-1':
-					ele[1].classList.remove('none')
-					ele[1].classList.add('grid')
-					ele[3].classList.remove('grid')
-					ele[3].classList.add('none')
-					ele[5].classList.remove('grid')
-					ele[5].classList.add('none')
-					break
-				case 'stage-2':
-					ele[1].classList.remove('grid')
-					ele[1].classList.add('none')
-					ele[3].classList.remove('none')
-					ele[3].classList.add('grid')
-					ele[5].classList.remove('grid')
-					ele[5].classList.add('none')
-					break
-				case 'stage-3':
-					ele[1].classList.remove('grid')
-					ele[1].classList.add('none')
-					ele[3].classList.remove('grid')
-					ele[3].classList.add('none')
-					ele[5].classList.remove('none')
-					ele[5].classList.add('grid')
-					break
-				default:
-					console.warn('Unexpected id:', id)
-					break
-			}
-		}
+// TODO: Extract
+const getStageForID = (id: string): FeaturesStage => {
+	switch (id) {
+		case FeaturesStageID.StageOne:
+			return FEATURES_STAGE_ONE
+
+		case FeaturesStageID.StageTwo:
+			return FEATURES_STAGE_TWO
+
+		case FeaturesStageID.StageThree:
+			return FEATURES_STAGE_THREE
+
+		default:
+			return FEATURES_STAGE_ONE // TODO: Refactor to return null here
 	}
+}
 
-	// On component mount
-	useEffect(() => {
-		accordian('stage-1')
+const Features: React.FC<void> = (): JSX.Element => {
+	const [cardWidth, setCardWidth] = useState<number>(0)
+	const [currentStageID, setCurrentStageID] = useState<FeaturesStageID>(
+		FeaturesStageID.StageOne
+	)
+
+	const currentStage = useMemo(() => (
+		getStageForID(currentStageID)
+	), [currentStageID])
+
+	const onStageClick = useCallback((id: string): void => {
+		setCurrentStageID(id as FeaturesStageID)
+		setCardWidth(0)
 	}, [])
 
 	return (
@@ -60,43 +50,47 @@ function Features (): JSX.Element {
 				<h2>Our <span>Features</span></h2>
 				<p className="des">Discover powerful tools that help you study.</p>
 			</div>
+
 			<div className="flex align-middle justify-end gap-2.5 mt-12 mx-0">
 				<div className="stages">
-					{stages.map(stage => (
-						<div key={stage.id} className={`stage ${stage.id === currentStage
-							? 'active-stage'
-							: ''}`} onClick={() => { accordian(stage.id) }}>
-							<h3>{stage.title}</h3>
-							<p>{stage.description}</p>
+					{FEATURES_STAGES.map(({ id, title, description }, i: number) => (
+						<div
+							key={`stage-${i}-${id}`}
+							onClick={onStageClick.bind(null, id)}
+							className={classNames('stage', {
+								'active-stage': id === currentStageID
+							})}
+						>
+							<h3>{title}</h3>
+							<p>{description}</p>
 						</div>
 					))}
 				</div>
 
-				<div className="owl-carousel-wrapper" ref={owlCarousel}>
-					{stages.map(stage => (
-						<React.Fragment key={stage.id}>
-							<div key={stage.id} className={'stage none'}
-								onClick={() => {
-									accordian(stage.id)
-								}}>
-								<h1>{stage.title}</h1>
-							</div>
+				<div className="owl-carousel-wrapper">
+					<div className="stage grid">
+						<h1>{currentStage.title}</h1>
+					</div>
 
-							<div className="owl-carousel">
-								<div className="cards" style={{
-									transform: `translateX(${cardWidth}px`
-								}} ref={cardsContainer}>
-									{stage.cards.map(card => (
-										<div key={card.title} className="card">
-											<Image className="card-image" src={card.image} alt={card.title} />
-											<h3 className="card-title">{card.title}</h3>
-											<p className="card-description">{card.description}</p>
-										</div>
-									))}
+					<div className="owl-carousel">
+						<div
+							className="cards"
+							style={{
+								transform: `translateX(${cardWidth}px`
+							}}
+						>
+							{currentStage.cards.map(({ title, description, image }, i: number) => (
+								<div
+									className="card"
+									key={`stage-card-${i}`}
+								>
+									<Image className="card-image" src={image} alt={title} />
+									<h3 className="card-title">{title}</h3>
+									<p className="card-description">{description}</p>
 								</div>
-							</div>
-						</React.Fragment>
-					))}
+							))}
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
