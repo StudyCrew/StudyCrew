6d53325c8f9a8f2eeae10cbd5f8c6e38f72b076d
Cris Mihalache <96000758+f3rno64@users.noreply.github.com>
2024-01-11 00:07:56 +0100

    Refactor/Fix: Features Component Mobile Layout (#149)
    * refactor: Features component, use classnames, extract + type stages, etc
    
    * fix: mobile layout of Features component, closes #125
    
    * fix: set Features props type to any for now
    
    * Revert "Merge branch 'develop' into 125-f3rno64-fix-features-section"
    
    This reverts commit 2eb1858d15209fbd0d178ffc907e2d2b65684030, reversing
    changes made to 10238822e404b7b707af61015fdae1ff470b8ea7.
    
    ---------
    
    Co-authored-by: Jacob <105601287+JacobHeldt@users.noreply.github.com>
    Co-authored-by: Jacob Heldt <jacobemil.heldt@gmail.com>

--- components/Features/Features.tsx
+++ components/Features/Features.tsx
@@ -1,58 +1,119 @@
-import React, { useRef, useState, useEffect } from 'react' // Removed useCallback
-import './Features.css'
-import stages from '../../lib/data/featureData'
 import Image from 'next/image'
+import classNames from 'classnames'
+import { useWindowSize } from '@uidotdev/usehooks'
+import React, { useMemo, useState, useCallback } from 'react'
+import {
+	FaChevronUp,
+	FaChevronLeft,
+	FaChevronDown,
+	FaChevronRight
+} from 'react-icons/fa'
 
-function Features (): JSX.Element {
-	const owlCarousel = useRef<HTMLDivElement | null>(null)
-	const cardsContainer = useRef<HTMLDivElement | null>(null)
-	const [cardWidth, setCardWidth] = useState(0)
-	const [currentStage, setCurrentStage] = useState('stage-1')
+import { type FeaturesStage, FeaturesStageID } from './types'
+import {
+	FEATURES_STAGES,
+	FEATURES_STAGE_ONE,
+	FEATURES_STAGE_TWO,
+	FEATURES_STAGE_THREE,
+	MOBILE_WIDTH_BREAKPOINT
+} from './const'
 
-	// Handle clicks on stages for larger screens
-	const accordian = (id: string): void => {
-		setCurrentStage(id)
+import './Features.css'
+
+// TODO: Extract
+const getStageForID = (id: string): FeaturesStage => {
+	switch (id) {
+		case FeaturesStageID.StageOne:
+			return FEATURES_STAGE_ONE
+
+		case FeaturesStageID.StageTwo:
+			return FEATURES_STAGE_TWO
+
+		case FeaturesStageID.StageThree:
+			return FEATURES_STAGE_THREE
+
+		default:
+			return FEATURES_STAGE_ONE // TODO: Refactor to return null here
+	}
+}
+
+const Features: React.FC<any> = (): JSX.Element => {
+	const { width: windowWidth } = useWindowSize()
+	const isMobile = windowWidth <= MOBILE_WIDTH_BREAKPOINT
+
+	const [cardWidth, setCardWidth] = useState<number>(0)
+	const [stageOpenStatuses, setStageOpenStatuses] = useState<boolean[]>([
+		false, false, false
+	])
+
+	const [stageActiveCardIndexes, setStageActiveCardIndexes] = useState<number[]>([
+		0, 0, 0
+	])
+
+	const [currentStageID, setCurrentStageID] = useState<FeaturesStageID>(
+		FeaturesStageID.StageOne
+	)
+
+	const currentStage = useMemo(() => (
+		getStageForID(currentStageID)
+	), [currentStageID])
+
+	const onStageClick = useCallback((id: string): void => {
+		setCurrentStageID(id as FeaturesStageID)
 		setCardWidth(0)
 
-		if (owlCarousel.current) {
-			const ele = owlCarousel.current.children
-			console.log(ele)
-			switch (id) {
-				case 'stage-1':
-					ele[1].classList.remove('none')
-					ele[1].classList.add('grid')
-					ele[3].classList.remove('grid')
-					ele[3].classList.add('none')
-					ele[5].classList.remove('grid')
-					ele[5].classList.add('none')
-					break
-				case 'stage-2':
-					ele[1].classList.remove('grid')
-					ele[1].classList.add('none')
-					ele[3].classList.remove('none')
-					ele[3].classList.add('grid')
-					ele[5].classList.remove('grid')
-					ele[5].classList.add('none')
-					break
-				case 'stage-3':
-					ele[1].classList.remove('grid')
-					ele[1].classList.add('none')
-					ele[3].classList.remove('grid')
-					ele[3].classList.add('none')
-					ele[5].classList.remove('none')
-					ele[5].classList.add('grid')
-					break
-				default:
-					console.warn('Unexpected id:', id)
-					break
+		const nextStageOpenStatuses = [...stageOpenStatuses]
+
+		if (id === FeaturesStageID.StageOne) {
+			nextStageOpenStatuses[0] = !nextStageOpenStatuses[0]
+		} else if (id === FeaturesStageID.StageTwo) {
+			nextStageOpenStatuses[1] = !nextStageOpenStatuses[1]
+		} else if (id === FeaturesStageID.StageThree) {
+			nextStageOpenStatuses[2] = !nextStageOpenStatuses[2]
+		}
+
+		setStageOpenStatuses(nextStageOpenStatuses)
+	}, [stageOpenStatuses])
+
+	const onNextStageCardClick = useCallback((id: string): void => {
+		const nextStageActiveCardIndexes = [...stageActiveCardIndexes]
+
+		if (id === FeaturesStageID.StageOne) {
+			if (nextStageActiveCardIndexes[0] < currentStage.cards.length - 1) {
+				nextStageActiveCardIndexes[0] += 1
+			}
+		} else if (id === FeaturesStageID.StageTwo) {
+			if (nextStageActiveCardIndexes[1] < currentStage.cards.length - 1) {
+				nextStageActiveCardIndexes[1] += 1
+			}
+		} else if (id === FeaturesStageID.StageThree) {
+			if (nextStageActiveCardIndexes[2] < currentStage.cards.length - 1) {
+				nextStageActiveCardIndexes[2] += 1
 			}
 		}
-	}
 
-	// On component mount
-	useEffect(() => {
-		accordian('stage-1')
-	}, [])
+		setStageActiveCardIndexes(nextStageActiveCardIndexes)
+	}, [stageActiveCardIndexes])
+
+	const onPrevStageCardClick = useCallback((id: string): void => {
+		const nextStageActiveCardIndexes = [...stageActiveCardIndexes]
+
+		if (id === FeaturesStageID.StageOne) {
+			if (nextStageActiveCardIndexes[0] > 0) {
+				nextStageActiveCardIndexes[0] -= 1
+			}
+		} else if (id === FeaturesStageID.StageTwo) {
+			if (nextStageActiveCardIndexes[1] > 0) {
+				nextStageActiveCardIndexes[1] -= 1
+			}
+		} else if (id === FeaturesStageID.StageThree) {
+			if (nextStageActiveCardIndexes[2] > 0) {
+				nextStageActiveCardIndexes[2] -= 1
+			}
+		}
+
+		setStageActiveCardIndexes(nextStageActiveCardIndexes)
+	}, [stageActiveCardIndexes])
 
 	return (
 		<div className="features">
@@ -60,44 +121,117 @@ function Features (): JSX.Element {
 				<h2>Our <span>Features</span></h2>
 				<p className="des">Discover powerful tools that help you study.</p>
 			</div>
-			<div className="flex align-middle justify-end gap-2.5 mt-12 mx-0">
-				<div className="stages">
-					{stages.map(stage => (
-						<div key={stage.id} className={`stage ${stage.id === currentStage
-							? 'active-stage'
-							: ''}`} onClick={() => { accordian(stage.id) }}>
-							<h3>{stage.title}</h3>
-							<p>{stage.description}</p>
-						</div>
-					))}
-				</div>
 
-				<div className="owl-carousel-wrapper" ref={owlCarousel}>
-					{stages.map(stage => (
-						<React.Fragment key={stage.id}>
-							<div key={stage.id} className={'stage none'}
-								onClick={() => {
-									accordian(stage.id)
-								}}>
-								<h1>{stage.title}</h1>
+			<div className={classNames("align-middle justify-end gap-2.5 mt-12 mx-0", {
+				flex: !isMobile,
+				'flex-column': isMobile
+			})}>
+				{!isMobile && (
+					<div className="stages">
+						{FEATURES_STAGES.map(({ id, title, description }, i: number) => (
+							<div
+								key={`stage-${i}-${id}`}
+								onClick={onStageClick.bind(null, id)}
+								className={classNames('stage', {
+									'active-stage': id === currentStageID
+								})}
+							>
+								<h3>{title}</h3>
+								<p>{description}</p>
 							</div>
+						))}
+					</div>
+				)}
 
-							<div className="owl-carousel">
-								<div className="cards" style={{
+				{!isMobile && (
+					<div className="owl-carousel-wrapper">
+						<div className="owl-carousel">
+							<div
+								className="cards"
+								style={{
 									transform: `translateX(${cardWidth}px`
-								}} ref={cardsContainer}>
-									{stage.cards.map(card => (
-										<div key={card.title} className="card">
-											<Image className="card-image" src={card.image} alt={card.title} />
-											<h3 className="card-title">{card.title}</h3>
-											<p className="card-description">{card.description}</p>
-										</div>
-									))}
+								}}
+							>
+								{currentStage.cards.map(({ title, description, image }, i: number) => (
+									<div
+										className="card"
+										key={`stage-card-${i}`}
+									>
+										<Image className="card-image" src={image} alt={title} />
+										<h3 className="card-title">{title}</h3>
+										<p className="card-description">{description}</p>
+									</div>
+								))}
+							</div>
+						</div>
+					</div>
+				)}
+
+				{isMobile && FEATURES_STAGES.map(({ id, title, description, cards }, i: number) => (
+					<div
+						key={`stage-${i}-${id}`}
+						className="mobile-stage-wrapper"
+					>
+						<div
+							className="stage"
+							onClick={onStageClick.bind(null, id)}
+						>
+							<h3>{title}</h3>
+							<p>{description}</p>
+						</div>
+
+						{stageOpenStatuses[i] && (
+							<div className="card-wrapper flex" key={`stage-card-${i}`}>
+								<div
+									onClick={onPrevStageCardClick.bind(null, id)}
+									className={classNames('flex stage-nav', {
+										disabled: stageActiveCardIndexes[i] === 0
+									})}
+								>
+									<FaChevronLeft size={48} />
+								</div>
+
+								<div className="card-inner-wrapper">
+									<div className="card">
+										<Image
+											className="card-image"
+											src={cards[stageActiveCardIndexes[i]].image}
+											alt={cards[stageActiveCardIndexes[i]].title}
+										/>
+
+										<h3 className="card-title">
+											{cards[stageActiveCardIndexes[i]].title}
+										</h3>
+
+										<p className="card-description">
+											{cards[stageActiveCardIndexes[i]].description}
+										</p>
+									</div>
+
+									<ul className="flex stage-nav-dots">
+										{cards.map((_, cardI: number) => (
+											<li
+												key={cardI}
+												className={classNames('stage-nav-dot', {
+													active: cardI === stageActiveCardIndexes[i]
+												})}
+											/>
+										))}
+									</ul>
+								</div>
+
+								<div
+									onClick={onNextStageCardClick.bind(null, id)}
+									className={classNames('flex stage-nav', {
+										disabled: stageActiveCardIndexes[i] === cards.length - 1
+									})}
+								>
+									<FaChevronRight size={48} />
 								</div>
 							</div>
-						</React.Fragment>
-					))}
-				</div>
+						)}
+					</div>
+				))}
 			</div>
 		</div>
 	)
