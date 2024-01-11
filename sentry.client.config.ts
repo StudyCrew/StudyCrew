bbe83ccf4b45b94ebaaa3e26810668152d793c13
Kyle Scudder <kyle@kylescudder.co.uk>
2024-01-11 12:03:01 +0000

    fix(exceptions): #176 fix sentry integration

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -10,8 +10,8 @@ Sentry.init({
 	// Adjust this value in production, or use tracesSampler for greater control
 	tracesSampleRate: 1,
 
-	// Setting this option to true will print useful information
-	// to the console while you're setting up Sentry.
+	// Setting this option to true will print useful information to the console while you're setting
+	// up Sentry.
 	debug: false,
 
 	replaysOnErrorSampleRate: 1.0,
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -10,7 +10,7 @@ Sentry.init({
 	// Adjust this value in production, or use tracesSampler for greater control
 	tracesSampleRate: 1,
 
-	// Setting this option to true will print useful information
-	// to the console while you're setting up Sentry.
+	// Setting this option to true will print useful information to the console while you're setting
+	// up Sentry.
 	debug: false
 })
