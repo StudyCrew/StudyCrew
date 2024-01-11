9c4b74894b260df56d1a5bbbe91003a53c9febe2
Kyle Scudder <74150974+kylescudder@users.noreply.github.com>
2024-01-11 14:08:41 +0000

    fix(sentry): #183 added middleware public route for sentry monitoring (#184)
    I also added the exception for all assets whilst I was here and this was also causing console errors.

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ import { authMiddleware } from '@clerk/nextjs'
 
 export default authMiddleware({
 	// An array of public routes that don't require authentication.
-	publicRoutes: ['/api/webhook/clerk'],
+	publicRoutes: ['/api/webhook/clerk', '/assets/(.*)', '/monitoring'],
 
 	// An array of routes to be ignored by the authentication middleware.
 	ignoredRoutes: ['/api/webhook/clerk', '/'],
