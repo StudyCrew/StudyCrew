8d9454652c5dd96630faea4551d5257b8a4cb9b0
Cris Mihalache <96000758+f3rno64@users.noreply.github.com>
2024-01-22 23:25:39 +0100

    Feature: Update Footer Design (#228)
    * feature: update footer design, links and waitlist action pending
    
    * feature: hook up footer links, waitlist input
    
    * fix: hook up remaining footer links, set target to _blank
    
    * fix: set footer link visited state color to black, rm duplicate link

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,23 +1,16 @@
-import React from 'react'
 import Image from 'next/image'
-import scrollToRef from '@/lib/hooks/scrollTo'
-import {
-  FaFacebookF,
-  FaInstagram,
-  FaLinkedin,
-  FaGithubSquare,
-  FaDiscord,
-  FaTiktok,
-  FaYoutube
-} from 'react-icons/fa'
+import _isEmpty from 'lodash/isEmpty'
+import React, { useState, useCallback } from 'react'
+import { FaYoutube, FaLinkedin, FaFacebookF, FaInstagram } from 'react-icons/fa'
 
+import scrollToRef from '@/lib/hooks/scrollTo'
 import Logo from '../../public/assets/Logo.svg'
+import { addToWaitlist } from '@/lib/actions/waitlist.actions'
 
 import { type FooterProps } from './types'
 
 const Footer: React.FC<FooterProps> = (props: FooterProps): JSX.Element => {
   const {
-    activePage,
     setActivePage,
     missionRef,
     featuresRef,
@@ -27,9 +20,49 @@ const Footer: React.FC<FooterProps> = (props: FooterProps): JSX.Element => {
     faqRef
   } = props
 
-  // Helper function to determine if a link is active
-  const isActive = (name: string): string =>
-    activePage === name ? 'activeNavLink' : ''
+  const [waitlistEmail, setWaitlistEmail] = useState<string>('')
+  const [waitlistErrorMessage, setWaitlistErrorMessage] = useState<string>('')
+  const onChangeWaitlistEmail = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setWaitlistEmail(e.target.value)
+    },
+    [setWaitlistEmail]
+  )
+
+  const handleJoinWaitlist = useCallback(async (): Promise<void> => {
+    try {
+      if (!waitlistEmail) {
+        setWaitlistErrorMessage('Please enter your email address.')
+        return
+      } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(waitlistEmail)
+      ) {
+        setWaitlistErrorMessage('Invalid email address')
+        return
+      }
+
+      const res = await addToWaitlist(waitlistEmail)
+
+      if (!res) {
+        setWaitlistErrorMessage('Failed to join waitlist. Please try again.')
+      } else {
+        setWaitlistErrorMessage('Successfully joined waitlist!')
+        setWaitlistEmail('')
+      }
+    } catch (error) {
+      setWaitlistErrorMessage('Failed to join waitlist. Please try again.')
+    }
+  }, [waitlistEmail, setWaitlistErrorMessage, setWaitlistEmail])
+
+  const onSubmitWaitlistEmail = useCallback(() => {
+    handleJoinWaitlist()
+      .then(() => {
+        setWaitlistEmail('')
+      })
+      .catch((err: Error) => {
+        console.error(err.stack)
+      })
+  }, [handleJoinWaitlist, waitlistEmail])
 
   // Handles the navigation link click based on the section's name
   const handleNavLinkClick = (name: string): void => {
@@ -60,102 +93,216 @@ const Footer: React.FC<FooterProps> = (props: FooterProps): JSX.Element => {
   }
 
   return (
-    <div className="bg-zircon-50 grid grid-cols-3 lg:grid-cols-5 gap-4 px-6 lg:px-[150px] py-6 items-center">
-      {/* Footer logo */}
-      <div className="flex items-center justify-center lg:justify-start col-span-1">
-        {/* SVG Logo */}
-        <Image alt="Logo" src={Logo} className="w-10" />
-        <h3 className="footer-title heading-font ml-4 font-semibold">
+    <div className="bg-zircon-50 flex-column lg:flex">
+      <div className="flex-column items-center lg:pl-16 lg:mr-16 py-6 lg:w-full">
+        <Image alt="Logo" src={Logo} className="mx-auto w-10" />
+        <h3 className="footer-title heading-font text-center mt-2 mb-0 font-semibold">
           StudyCrew
         </h3>
-      </div>
-
-      {/* Footer menu */}
-      <div className="col-span-1 lg:col-span-2 ml-12">
-        <ul className="flex justify-center lg:justify-start gap-4">
-          <li
-            className={`${isActive('mission')} hover:cursor-pointer`}
-            onClick={() => {
-              handleNavLinkClick('mission')
-              setActivePage('mission')
-            }}
-          >
-            Mission
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="mailto:info@studycrew.world"
+          className="block text-center text-sm mb-4 text-black visited:text-black hover:text-black"
+        >
+          info@studycrew.world
+        </a>
+        <ul className="flex justify-center mx-auto">
+          <li className="mr-4">
+            <a
+              href="https://www.facebook.com/profile.php?id=61555998230454"
+              className="text-black visited:text-black hover:text-black"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <FaFacebookF size={20} />
+            </a>
           </li>
-          <li
-            className={`${isActive('features')} hover:cursor-pointer`}
-            onClick={() => {
-              handleNavLinkClick('features')
-              setActivePage('features')
-            }}
-          >
-            Features
+          <li className="mr-4">
+            <a
+              href="https://www.youtube.com/channel/UCpeI7Q-WPZ88Uv1KCqs814Q"
+              className="text-black visited:text-black hover:text-black"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <FaYoutube size={20} />
+            </a>
           </li>
-          <li
-            className={`${isActive('project')} hover:cursor-pointer`}
-            onClick={() => {
-              handleNavLinkClick('project')
-              setActivePage('project')
-            }}
-          >
-            Project
+          <li className="mr-4">
+            <a
+              href="https://www.instagram.com/studycrew.world/"
+              className="text-black visited:text-black hover:text-black"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <FaInstagram size={20} />
+            </a>
           </li>
-          <li
-            className={`${isActive('signup')} hover:cursor-pointer`}
-            onClick={() => {
-              handleNavLinkClick('signup')
-              setActivePage('signup')
-            }}
-          >
-            Sign Up
-          </li>
-          <li
-            className={`${isActive('team')} hover:cursor-pointer`}
-            onClick={() => {
-              handleNavLinkClick('team')
-              setActivePage('team')
-            }}
-          >
-            Team
-          </li>
-          <li
-            className={`${isActive('faq')} hover:cursor-pointer`}
-            onClick={() => {
-              handleNavLinkClick('faq')
-              setActivePage('faq')
-            }}
-          >
-            FAQs
+          <li>
+            <a
+              href="https://www.linkedin.com/company/studycrewofficial"
+              className="text-black visited:text-black hover:text-black"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <FaLinkedin size={20} />
+            </a>
           </li>
         </ul>
-        <hr />
       </div>
 
-      {/* Social media icons */}
-      <div className="col-span-1 lg:col-span-2">
-        <ul className="flex justify-center lg:justify-end gap-4">
-          <li>
-            <FaGithubSquare size={20} />
+      <div className="lg:mr-16 py-6 flex-column text-center lg:text-left">
+        <h3 className="font-bold mt-0 mb-2">Navigation</h3>
+        <ul className="flex-column">
+          <li className="hover:underline hover:cursor-pointer">
+            <a
+              href="#"
+              className="text-black hover:underline hover:cursor-pointer hover:text-black visited:text-black no-underline"
+              onClick={() => {
+                handleNavLinkClick('mission')
+                setActivePage('mission')
+              }}
+            >
+              Mission
+            </a>
           </li>
-          <li>
-            <FaInstagram size={20} />
+          <li className="hover:underline hover:cursor-pointer">
+            <a
+              href="#"
+              className="text-black hover:underline hover:cursor-pointer hover:text-black visited:text-black no-underline"
+              onClick={() => {
+                handleNavLinkClick('features')
+                setActivePage('features')
+              }}
+            >
+              Features
+            </a>
           </li>
-          <li>
-            <FaYoutube size={20} />
+          <li className="hover:underline hover:cursor-pointer">
+            <a
+              href="#"
+              className="text-black hover:underline hover:cursor-pointer hover:text-black visited:text-black no-underline"
+              onClick={() => {
+                handleNavLinkClick('project')
+                setActivePage('project')
+              }}
+            >
+              Project
+            </a>
           </li>
-          <li>
-            <FaFacebookF size={20} />
+          <li className="hover:underline hover:cursor-pointer">
+            <a
+              href="#"
+              className="text-black hover:underline hover:cursor-pointer hover:text-black visited:text-black no-underline"
+              onClick={() => {
+                handleNavLinkClick('signup')
+                setActivePage('signup')
+              }}
+            >
+              Sign Up
+            </a>
           </li>
-          <li>
-            <FaLinkedin size={20} />
+          <li className="hover:underline hover:cursor-pointer">
+            <a
+              href="#"
+              className="text-black hover:underline hover:cursor-pointer hover:text-black visited:text-black no-underline"
+              onClick={() => {
+                handleNavLinkClick('team')
+                setActivePage('team')
+              }}
+            >
+              Team
+            </a>
           </li>
-          <li>
-            <FaTiktok size={20} />
-          </li>
-          <li>
-            <FaDiscord size={20} />
+          <li className="hover:underline hover:cursor-pointer">
+            <a
+              href="#"
+              className="text-black hover:underline hover:cursor-pointer hover:text-black visited:text-black no-underline"
+              onClick={() => {
+                handleNavLinkClick('faq')
+                setActivePage('faq')
+              }}
+            >
+              FAQ
+            </a>
           </li>
         </ul>
+      </div>
+
+      <div className="lg:mr-16 py-6 text-center lg:text-left">
+        <h3 className="font-bold mt-0 mb-2">Contribute</h3>
+        <ul className="flex-column">
+          <li className="hover:underline hover:cursor-pointer">
+            <a
+              className="text-black hover:underline hover:cursor-pointer hover:text-black visited:text-black no-underline"
+              href="https://github.com/StudyCrew"
+              rel="noreferrer"
+              target="_blank"
+            >
+              Github
+            </a>
+          </li>
+          <li className="hover:underline hover:cursor-pointer">
+            <a
+              className="text-black hover:underline hover:cursor-pointer hover:text-black visited:text-black no-underline"
+              href="https://discord.gg/j5d7PMes"
+              rel="noreferrer"
+              target="_blank"
+            >
+              Discord
+            </a>
+          </li>
+          <li className="hover:underline hover:cursor-pointer">
+            <a
+              className="text-black hover:underline hover:cursor-pointer hover:text-black visited:text-black no-underline"
+              href="https://forms.gle/CBKSjovcWeRGWbg2A"
+              rel="noreferrer"
+              target="_blank"
+            >
+              Join Team
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="lg:mr-16 py-6 text-center lg:text-left">
+        <h3 className="font-bold mt-0 mb-2">Collaborations</h3>
+        <ul className="flex-column">
+          <li className="hover:underline hover:cursor-pointer">
+            <a
+              className="text-black hover:underline hover:cursor-pointer hover:text-black visited:text-black no-underline"
+              href="https://aquin.app/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              Aquin
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="flex-column lg:px-16 bg-blue-200 pt-6 pb-8 w-full text-center lg:text-left">
+        <h3 className="font-bold mt-0 mb-2">Sign Up</h3>
+        <p className="text-sm mb-4">
+          Ready to transform Your Learning Experience?
+        </p>
+        <input
+          type="email"
+          value={waitlistEmail}
+          placeholder="Enter your email"
+          onChange={onChangeWaitlistEmail}
+          className="mr-4 lg:mr-0 mb-4 border-2 border-blue-300 px-4 py-2 rounded-lg px-4 w-3/4"
+        />
+        <button
+          onClick={onSubmitWaitlistEmail}
+          className="text-white bg-blue-500 px-4 rounded-lg text-center py-2"
+        >
+          Join Waitlist
+        </button>
+
+        {!_isEmpty(waitlistErrorMessage) && (
+          <p className="text-red-500 text-sm mt-4">{waitlistErrorMessage}</p>
+        )}
       </div>
     </div>
   )
