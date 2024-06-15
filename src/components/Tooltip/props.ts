8b16ed6571d7db571c12c71890fe564553909c09
Divyesh Parmar <divyeshpar2496@gmail.com>
2024-06-16 01:37:52 +0530

    370: removed the tooltip folder (#371)

--- src/components/Tooltip/props.ts
+++
@@ -1,6 +0,0 @@
-export interface TooltipProps {
-  information: string
-  textHover: string
-  align?: 'center' | 'start' | 'end'
-  side?: 'bottom' | 'top' | 'right' | 'left'
-}
--- src/components/Tooltip/types.ts
+++
@@ -1,4 +0,0 @@
-export interface TooltipTypes {
-  className?: string
-  sideOffset?: number
-}
