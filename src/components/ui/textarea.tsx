b2e0db63e0ebb10448ab34bb3bfb45d606e95996
s3822042 <s3822042@rmit.edu.vn>
2024-06-15 15:02:49 +0700

    fix(textarea): match with Figma design

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -2,20 +2,19 @@ import * as React from 'react'
 import { type TextareaProps } from '@/interface/controls/textarea'
 
 import { cn } from '@/utils'
-import Label from '@/components/ui/label'
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, ...props }, ref) => {
     return (
       <>
-        <Label htmlFor={props.label}>{props.label}</Label>
         <textarea
+          placeholder={props.label}
           className={cn(
-            `flex min-h-[80px] w-full rounded-md border border-slate-200 bg-white px-3 py-2
-            text-sm ring-offset-white placeholder:text-slate-500 focus-visible:outline-none
+            `flex min-h-[80px] w-full rounded-[1px] border border-[#98A2B3] bg-white px-3 py-2
+            text-sm ring-offset-white placeholder:text-[#98A2B3] focus-visible:outline-none
             focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2
             disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800
-            dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400
+            dark:bg-[#98A2B3] dark:ring-offset-slate-950 dark:placeholder:text-slate-400
             dark:focus-visible:ring-slate-300`,
             className
           )}
