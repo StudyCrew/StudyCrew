1a691e55266c74005fb4b08c0c433035e1f48fd7
Jacob Heldt <jacobemil.heldt@gmail.com>
2024-07-14 16:03:16 -0700

    Update team members section

--- src/app/(landing)/_components/Team/TeamCard.tsx
+++ src/app/(landing)/_components/Team/TeamCard.tsx
@@ -10,7 +10,7 @@ interface TeamCardProps {
 const TeamCard = ({ name, avatar, role }: TeamCardProps): JSX.Element => {
   return (
     <div className="flex-none p-[10px] py-[50px] m-[10px] grid place-items-center min-w-[280px] rounded-[20px] bg-white/70 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] backdrop-blur-[25px]">
-      <Image className="max-w-[110px] w-full" src={avatar} alt={name} />
+      <Image className="max-w-[110px] w-full rounded-full bg-zircon-300" src={avatar} alt={name} />
       <h5 className="text-[25px] m-[15px]">{name}</h5>
       <p className="">{role}</p>
     </div>
