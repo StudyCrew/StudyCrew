f0800f2b540fb028fc766c9860c11dd8e2f58e98
Jacob <105601287+JacobHeldt@users.noreply.github.com>
2024-07-14 16:04:42 -0700

    434 update team members section (#435)
    * Update team members section
    
    * Update linting

--- src/data/team_members.ts
+++ src/data/team_members.ts
@@ -1,25 +1,28 @@
-import dew from 'public/assets/avatars/dew.svg' assert { type: 'svg' }
-import zai from 'public/assets/avatars/zai.svg' assert { type: 'svg' }
-import jona from 'public/assets/avatars/jona.svg' assert { type: 'svg' }
-import kian from 'public/assets/avatars/kian.svg' assert { type: 'svg' }
-import aiya from 'public/assets/avatars/aiya.svg' assert { type: 'svg' }
-import kyle from 'public/assets/avatars/kyle.svg' assert { type: 'svg' }
 import jacob from 'public/assets/avatars/jacob.svg' assert { type: 'svg' }
-import sarah from 'public/assets/avatars/sarah.svg' assert { type: 'svg' }
+import anmol from 'public/assets/avatars/anmol.svg' assert { type: 'svg' }
+import user from 'public/assets/avatars/user.svg' assert { type: 'svg' }
+import rohan from 'public/assets/avatars/rohan.svg' assert { type: 'svg' }
+import oviya from 'public/assets/avatars/oviya.svg' assert { type: 'svg' }
+import adedayo from 'public/assets/avatars/adedayo.svg' assert { type: 'svg' }
+import tarunvidyut from 'public/assets/avatars/tarunvidyut.svg' assert { type: 'svg' }
+import toryn from 'public/assets/avatars/toryn.svg' assert { type: 'svg' }
 import xavier from 'public/assets/avatars/xavier.svg' assert { type: 'svg' }
-import melody from 'public/assets/avatars/melody.svg' assert { type: 'svg' }
-import vishal from 'public/assets/avatars/vishal.svg' assert { type: 'svg' }
-import nicholas from 'public/assets/avatars/nicholas.svg' assert { type: 'svg' }
-import sugumaran from 'public/assets/avatars/sugumaran.svg' assert { type: 'svg' }
+import daksh from 'public/assets/avatars/daksh.svg' assert { type: 'svg' }
 
 enum TeamMemberRole {
   Founder = 'Founder',
+  COO = 'COO',
+  HeadMarketing = 'Head of Marketing',
   Marketing = 'Marketing',
-  Developer = 'Developer',
+  HeadDevelopment = 'Head of Development',
   Development = 'Development',
+  HeadUIUXDesign = 'Head of UI/UX Design',
   UIUXDesign = 'UI/UX Design',
-  LeadDeveloper = 'Lead Developer',
-  ProductManagement = 'Product Management'
+  HeadProductManagement = 'Head of Product Management',
+  ProductManagement = 'Product Management',
+  HeadContentManagement = 'Head of Content Management',
+  ContentManagement = 'Content Management',
+  Kian = 'Lead & Homepage Designer'
 }
 
 const TEAM_MEMBERS = [
@@ -29,24 +32,19 @@ const TEAM_MEMBERS = [
     avatar: jacob as string
   },
   {
-    name: 'Jona',
-    role: TeamMemberRole.Marketing,
-    avatar: jona as string
+    name: 'Anmol',
+    role: TeamMemberRole.COO,
+    avatar: anmol as string
   },
   {
-    name: 'Sugumaran',
-    role: TeamMemberRole.Development,
-    avatar: sugumaran as string
+    name: 'Amina',
+    role: TeamMemberRole.HeadMarketing,
+    avatar: user as string
   },
   {
-    name: 'Kian',
-    role: TeamMemberRole.UIUXDesign,
-    avatar: kian as string
-  },
-  {
-    name: 'Abiel',
-    role: TeamMemberRole.Development,
-    avatar: sugumaran as string
+    name: 'Rohan',
+    role: TeamMemberRole.HeadContentManagement,
+    avatar: rohan as string
   },
   {
     name: 'Xavier',
@@ -54,44 +52,79 @@ const TEAM_MEMBERS = [
     avatar: xavier as string
   },
   {
-    name: 'Dew',
-    role: TeamMemberRole.UIUXDesign,
-    avatar: dew as string
-  },
-  {
-    name: 'Nicholas',
-    role: TeamMemberRole.Marketing,
-    avatar: nicholas as string
-  },
-  {
-    name: 'Melody',
-    role: TeamMemberRole.UIUXDesign,
-    avatar: melody as string
-  },
-  {
-    name: 'Vishal',
-    role: TeamMemberRole.Development,
-    avatar: vishal as string
-  },
-  {
-    name: 'Kyle',
-    role: TeamMemberRole.LeadDeveloper,
-    avatar: kyle as string
+    name: 'Daksh',
+    role: TeamMemberRole.ProductManagement,
+    avatar: daksh as string
   },
   {
     name: 'Aiya',
     role: TeamMemberRole.ProductManagement,
-    avatar: aiya as string
+    avatar: user as string
+  },
+  {
+    name: 'Oviya',
+    role: TeamMemberRole.Marketing,
+    avatar: oviya as string
+  },
+  {
+    name: 'Jona',
+    role: TeamMemberRole.Marketing,
+    avatar: user as string
+  },
+  {
+    name: 'Tarunvidyut',
+    role: TeamMemberRole.ContentManagement,
+    avatar: tarunvidyut as string
+  },
+  {
+    name: 'Kian',
+    role: TeamMemberRole.Kian,
+    avatar: user as string
+  },
+  {
+    name: 'Adedayo',
+    role: TeamMemberRole.UIUXDesign,
+    avatar: adedayo as string
+  },
+  {
+    name: 'Dew',
+    role: TeamMemberRole.UIUXDesign,
+    avatar: user as string
+  },
+  {
+    name: 'Melody',
+    role: TeamMemberRole.UIUXDesign,
+    avatar: user as string
   },
   {
     name: 'Sarah',
     role: TeamMemberRole.UIUXDesign,
-    avatar: sarah as string
+    avatar: user as string
+  },
+  {
+    name: 'Vishal',
+    role: TeamMemberRole.Development,
+    avatar: user as string
+  },
+  {
+    name: 'Toryn',
+    role: TeamMemberRole.Development,
+    avatar: toryn as string
+  },
+  {
+    name: 'Kyle',
+    role: TeamMemberRole.Development,
+    avatar: user as string
+  },
+  {
+    name: 'Abiel',
+    role: TeamMemberRole.Development,
+    avatar: user as string
   },
   {
     name: 'Zai',
-    role: TeamMemberRole.Developer,
-    avatar: zai as string
+    role: TeamMemberRole.Development,
+    avatar: user as string
   }
 ]
 
