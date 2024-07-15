939a3983627fed705fdcf95232cd93f96dc0f6e6
Jacob Heldt <jacobemil.heldt@gmail.com>
2024-07-14 17:05:25 -0700

    Resolve Vercel error

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -1,21 +1,15 @@
-'use client'
-
 import * as React from 'react'
 import * as TooltipPrimitive from '@radix-ui/react-tooltip'
-import type { TooltipTypes } from '@/components/Tooltip/types'
 import { cn } from '@/utils'
 
 const TooltipProvider = TooltipPrimitive.Provider
-
 const Tooltip = TooltipPrimitive.Root
-
 const TooltipTrigger = TooltipPrimitive.Trigger
-
 const TooltipArrow = TooltipPrimitive.Arrow
 
 const TooltipContent = React.forwardRef<
   React.ElementRef<typeof TooltipPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content> & TooltipTypes
+  React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content> & { className?: string; sideOffset?: number }
 >(({ className, sideOffset = 4, ...props }, ref) => (
   <TooltipPrimitive.Content
     ref={ref}
