432ada51ba59fbd6f6c011212ac9384b0f768e4c
Mohit Kamlesh Panchal <23110208@iitgn.ac.in>
2024-07-31 00:12:14 +0530

    implement material component

--- src/app/(core)/dashboard/page.tsx
+++ src/app/(core)/dashboard/page.tsx
@@ -1,10 +1,7 @@
-import MaterialComponent from '@/components/widgets/MaterialComponent'
-import { MaterialType } from '../../../../types'
-
 export default function Dashboard(): JSX.Element {
   return (
     <div>
-      <MaterialComponent type={MaterialType.Word} user={"Mohit Kamlesh Panchal"} date={new Date()} link={"https://youtu.be/aoB2CiFNGdc?si=dA_2933_oOypEBfj"} title={"Homework"} websiteLink={"/groups"} />
+      Dashboard
     </div>
   )
 }
