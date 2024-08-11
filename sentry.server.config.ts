507adc53b053406ec6ea78dbcdf1738bc5ba1b2f
Zai <130903099+zaibutcooler@users.noreply.github.com>
2024-08-12 03:00:52 +0630

    get rid of sentry

--- sentry.server.config.ts
+++
@@ -1,7 +0,0 @@
-import * as Sentry from '@sentry/nextjs'
-
-Sentry.init({
-  dsn: 'https://54f6437cf21fd3580c3e78da0943237b@o4506271642812416.ingest.sentry.io/4506271644319744',
-  tracesSampleRate: 1,
-  debug: false
-})
