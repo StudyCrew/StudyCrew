baf11dab94e2f0958a85ae5fc91b7ac7273a7f52
Jacob Heldt <jacobemil.heldt@gmail.com>
2024-08-16 18:23:41 +0200

    Improve linting

--- src/app/check/page.tsx
+++ src/app/check/page.tsx
@@ -1,23 +1,32 @@
-"use client";
-import Dropdown from '@/components/widgets/dropdown/Dropdown';
-import React, { useState } from 'react';
+'use client'
+import Dropdown from '@/components/widgets/dropdown/Dropdown'
+import React, { useState } from 'react'
 
 const Check: React.FC = () => {
-    const [opened, setOpened] = useState<boolean>(false);
-    const [currentChoice, setCurrentChoice] = useState<string>("Maths");
-    const choices: string[] = ["English", "Math", "Science", "Social Science", "Computer Science", "Geography", "Chemistry", "Physics"];
+  const [opened, setOpened] = useState<boolean>(false)
+  const [currentChoice, setCurrentChoice] = useState<string>('Maths')
+  const choices: string[] = [
+    'English',
+    'Math',
+    'Science',
+    'Social Science',
+    'Computer Science',
+    'Geography',
+    'Chemistry',
+    'Physics'
+  ]
 
-    return (
-        <div className="flex justify-center items-center h-screen">
-            <Dropdown
-                opened={opened}
-                setOpened={setOpened}
-                currentChoice={currentChoice}
-                setCurrentChoice={setCurrentChoice}
-                choices={choices}
-            />
-        </div>
-    );
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Dropdown
+        opened={opened}
+        setOpened={setOpened}
+        currentChoice={currentChoice}
+        setCurrentChoice={setCurrentChoice}
+        choices={choices}
+      />
+    </div>
+  )
 }
 
-export default Check;
+export default Check
--- src/components/widgets/dropdown/Dropdown.tsx
+++ src/components/widgets/dropdown/Dropdown.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React from 'react'
 
 type DropdownProps = {
-  opened: boolean;
-  setOpened: (opened: boolean) => void;
-  currentChoice: string;
-  setCurrentChoice: (choice: string) => void;
-  choices: string[];
-};
+  opened: boolean
+  setOpened: (opened: boolean) => void
+  currentChoice: string
+  setCurrentChoice: (choice: string) => void
+  choices: string[]
+}
 
-const Dropdown: React.FC<DropdownProps> = ({ opened, setOpened, currentChoice, setCurrentChoice, choices }) => {
+const Dropdown: React.FC<DropdownProps> = ({
+  opened,
+  setOpened,
+  currentChoice,
+  setCurrentChoice,
+  choices
+}) => {
   return (
-    <div className="dropdown flex flex-col relative" style={{ fontFamily: 'DM Sans, sans-serif' }}>
+    <div
+      className="dropdown flex flex-col relative"
+      style={{ fontFamily: 'DM Sans, sans-serif' }}
+    >
       <style>
         {`
           @import url('https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap');
@@ -24,27 +33,40 @@ const Dropdown: React.FC<DropdownProps> = ({ opened, setOpened, currentChoice, s
           }
         `}
       </style>
-      <div className={`currentchoice flex py-2 px-2 text-xl w-fit border-2 border-[#E5E5E5] ${opened ? "rounded-t-[6px]" : "rounded-[6px]"}`}>
+      <div
+        className={`currentchoice flex py-2 px-2 text-xl w-fit border-2 border-[#E5E5E5] ${opened ? 'rounded-t-[6px]' : 'rounded-[6px]'}`}
+      >
         <div className="text w-[300px]">{currentChoice}</div>
         <div
-          className={`chevron w-[28px] h-[28px] justify-center flex items-center transition-all cursor-pointer ${opened ? "-rotate-90" : "rotate-90"}`}
-          onClick={() => { setOpened(!opened); }}
+          className={`chevron w-[28px] h-[28px] justify-center flex items-center transition-all cursor-pointer ${opened ? '-rotate-90' : 'rotate-90'}`}
+          onClick={() => {
+            setOpened(!opened)
+          }}
         >
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#000000" viewBox="0 0 256 256">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            fill="#000000"
+            viewBox="0 0 256 256"
+          >
             <path d="M181.66,133.66l-80,80a8,8,0,0,1-11.32-11.32L164.69,128,90.34,53.66a8,8,0,0,1,11.32-11.32l80,80A8,8,0,0,1,181.66,133.66Z"></path>
           </svg>
         </div>
       </div>
       {opened && (
-        <div className="w-[332px] bg-[#f3f4f8] absolute top-[46px] rounded rounded-b-[6px] p-2 flex flex-col" style={{ fontFamily: 'DM Sans, sans-serif' }}>
+        <div
+          className="w-[332px] bg-[#f3f4f8] absolute top-[46px] rounded rounded-b-[6px] p-2 flex flex-col"
+          style={{ fontFamily: 'DM Sans, sans-serif' }}
+        >
           <div className="flex flex-col h-[200px] overflow-y-auto">
             {choices.map((option, i) => (
               <button
                 className="p-2 py-1 text-left rounded-md transition-all hover:bg-[#3A86FF] hover:text-white"
                 key={i}
                 onClick={() => {
-                  setCurrentChoice(option);
-                  setOpened(false);
+                  setCurrentChoice(option)
+                  setOpened(false)
                 }}
                 style={{ fontFamily: 'DM Sans, sans-serif' }}
               >
@@ -55,7 +77,7 @@ const Dropdown: React.FC<DropdownProps> = ({ opened, setOpened, currentChoice, s
         </div>
       )}
     </div>
-  );
+  )
 }
 
-export default Dropdown;
+export default Dropdown
