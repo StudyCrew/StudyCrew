12e2a5e69057c7b1d67ede785d23a0a7d1313fd9
Jacob Heldt <jacobemil.heldt@gmail.com>
2024-08-17 10:36:26 +0200

    Clean up check file and adjust font sizes

--- src/app/check/page.tsx
+++ src/app/check/page.tsx
@@ -1,77 +1,8 @@
-"use client"
-import NotificationCard from '@/components/widgets/notifications/NotificationCard'
 import React from 'react'
-
-interface Variant1Data {
-  size: string;
-  content: string;
-  title: string;
-}
-
-interface Variant2Data {
-  size: string;
-  groupName: string;
-  img: string;
-  username: string;
-  date: string;
-  question: string;
-}
-
-interface Variant3Data {
-  size: string;
-  groupName: string;
-  img: string;
-  username: string;
-  date: string;
-  assignment: string;
-  onClick?: () => void;
-}
 
 const Check: React.FC = () => {
   return (
-    <div className="flex flex-col justify-evenly items-center min-h-screen bg-[#e4e7ec] gap-x-4">
-      <div className="flex gap-x-4">
-        <NotificationCard variant="platform" variant1Data={{ size: "small", content: "hello i am", title: "Hello" }} />
-        <NotificationCard variant="platform" variant1Data={{ size: "large", content: "hello i am", title: "Hello" }} />
-      </div>
-      <div className="flex gap-x-4">
-        <NotificationCard variant="grpQuestion" variant2Data={{
-          size: "small",
-          groupName: "AP Calc",
-          img: "https://s3-alpha-sig.figma.com/img/8984/cd4d/716f2115474431e82106834a5708dd89?Expires=1724630400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=KyelsmEvilnxxSFdbtqSpzfIpkbb07OzhFrcUMKg0AqGEfZ8hHBViue3iqTWkr8EANoDhIeTe72WM70TsA8IgGtptxtiMeIv-kS0G4FaQW2aA67zIx8Hp05I9VFMwJsgkOn-QLtA6ZqpjXaMHJ1QuB8MsepMZtnAlousJkFzZA5Qww0qdoTDTaiHvFel35U0yt~qD6mWq19LXwvWoEne2Kg7wuOEhkgQV3iqd4UnfPb3Pscn830GRWEK4HdveWkhgQepBBEjv91x2KLRgdoOf56~qAgInImfKnZ5CBKRmIxoFUWrv3BYsTvgnUReyLos1eqIMEo5~Ji~AUZeLAqUqw__",
-          username: "Agatha Lewis-Johanssen",
-          date: "Aug 23 12:24 PM",
-          question: "What are the key factors in social change?"
-        }} />
-        <NotificationCard variant="grpQuestion" variant2Data={{
-          size: "large",
-          groupName: "AP Calc",
-          img: "https://s3-alpha-sig.figma.com/img/8984/cd4d/716f2115474431e82106834a5708dd89?Expires=1724630400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=KyelsmEvilnxxSFdbtqSpzfIpkbb07OzhFrcUMKg0AqGEfZ8hHBViue3iqTWkr8EANoDhIeTe72WM70TsA8IgGtptxtiMeIv-kS0G4FaQW2aA67zIx8Hp05I9VFMwJsgkOn-QLtA6ZqpjXaMHJ1QuB8MsepMZtnAlousJkFzZA5Qww0qdoTDTaiHvFel35U0yt~qD6mWq19LXwvWoEne2Kg7wuOEhkgQV3iqd4UnfPb3Pscn830GRWEK4HdveWkhgQepBBEjv91x2KLRgdoOf56~qAgInImfKnZ5CBKRmIxoFUWrv3BYsTvgnUReyLos1eqIMEo5~Ji~AUZeLAqUqw__",
-          username: "Agatha Lewis-Johanssen",
-          date: "Aug 23 12:24 PM",
-          question: "What are the key factors in social change?"
-        }} />
-      </div>
-      <div className="flex gap-x-4">
-        <NotificationCard variant="grpMaterial" variant3Data={{
-          size: "small",
-          groupName: "Biochemistry",
-          img: "https://s3-alpha-sig.figma.com/img/817b/b0f2/2e6ea37d5b1c29713b7d4b9c564d76af?Expires=1724630400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=YMRLKNTjXJKJ1PuNE1-1obKuEMr9XKF1qOWnR9S6hfNPxhwS7fjFNR4-yEF2n4typu9PqxsNeC4ZYJwtjojmyXdyfSWShgHvoiXW04NDxD8pgj76ESKxJMrRqycZlJLgGDPp1-qCcal1rzgn-y4RybNCmwN5YTcB5CZ83qA1pfY9Z7TVvin8Q6eFHqy4iNx9NyUSC6QqHc4tq5a~3D~wYFXCNG-xY-hvcY~9ErKP1jvIEsd3f4robLGUQU~kz-dG0PcTMc1yrRudO8jzsPsTh1Wnb~2~L5C-uBh9nBTyPYsE2JZbw-SIsH-ujsNFzLHKSVaF5eblfS6UH1ioDHd6eA__",
-          username: "Harry Kane",
-          date: "Aug 23 12:24 PM",
-          assignment: "Homework assignment Chapter 3.3 #1-28"
-        }} />
-        <NotificationCard variant="grpMaterial" variant3Data={{
-          size: "large",
-          groupName: "Biochemistry",
-          img: "https://s3-alpha-sig.figma.com/img/817b/b0f2/2e6ea37d5b1c29713b7d4b9c564d76af?Expires=1724630400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=YMRLKNTjXJKJ1PuNE1-1obKuEMr9XKF1qOWnR9S6hfNPxhwS7fjFNR4-yEF2n4typu9PqxsNeC4ZYJwtjojmyXdyfSWShgHvoiXW04NDxD8pgj76ESKxJMrRqycZlJLgGDPp1-qCcal1rzgn-y4RybNCmwN5YTcB5CZ83qA1pfY9Z7TVvin8Q6eFHqy4iNx9NyUSC6QqHc4tq5a~3D~wYFXCNG-xY-hvcY~9ErKP1jvIEsd3f4robLGUQU~kz-dG0PcTMc1yrRudO8jzsPsTh1Wnb~2~L5C-uBh9nBTyPYsE2JZbw-SIsH-ujsNFzLHKSVaF5eblfS6UH1ioDHd6eA__",
-          username: "Harry Kane",
-          date: "Aug 23 12:24 PM",
-          assignment: "Homework assignment Chapter 3.3 #1-28",
-          onClick: () => { alert("hello") }
-        }} />
-      </div>
-    </div>
+    <div></div>
   )
 }
 
--- src/components/widgets/notifications/NotificationCard.tsx
+++ src/components/widgets/notifications/NotificationCard.tsx
@@ -44,11 +44,11 @@ const NotificationCard: React.FC<NotificationCardProps> = ({variant, variant1Dat
                 <path d="M226.76,69a8,8,0,0,0-12.84-2.88l-40.3,37.19-17.23-3.7-3.7-17.23,37.19-40.3A8,8,0,0,0,187,29.24,72,72,0,0,0,88,96,72.34,72.34,0,0,0,94,124.94L33.79,177c-.15.12-.29.26-.43.39a32,32,0,0,0,45.26,45.26c.13-.13.27-.28.39-.42L131.06,162A72,72,0,0,0,232,96,71.56,71.56,0,0,0,226.76,69ZM160,152a56.14,56.14,0,0,1-27.07-7,8,8,0,0,0-9.92,1.77L67.11,211.51a16,16,0,0,1-22.62-22.62L109.18,133a8,8,0,0,0,1.77-9.93,56,56,0,0,1,58.36-82.31l-31.2,33.81a8,8,0,0,0-1.94,7.1L141.83,108a8,8,0,0,0,6.14,6.14l26.35,5.66a8,8,0,0,0,7.1-1.94l33.81-31.2A56.06,56.06,0,0,1,160,152Z"></path>
               </svg>
             </div>
-            <div className={`${variant1Data.size === "small" ? "w-[240px]" : "w-[422px]"} bg-[#eecc51] flex items-center h-[40px] px-4 py-2 text-[21px] rounded-tr-xl`} style={{boxSizing:"border-box"}}>
+            <div className={`${variant1Data.size === "small" ? "w-[240px]" : "w-[422px]"} bg-[#eecc51] flex items-center h-[40px] px-4 py-2 text-[16px] rounded-tr-xl`} style={{boxSizing:"border-box"}}>
               {variant1Data.title}
             </div>
           </div>
-          <div className="px-4 py-2 text-lg rounded-b-xl bg-white">
+          <div className="px-4 py-2 text-lg rounded-b-xl bg-white text-[14px]">
             {variant1Data.content}
           </div>
         </div>
@@ -57,22 +57,22 @@ const NotificationCard: React.FC<NotificationCardProps> = ({variant, variant1Dat
         <div className={`${variant2Data.size==="small"?"w-[280px]":"w-[462px]"} flex flex-col`}>
           <div className="h-[40px] flex bg-[#64a1ff] rounded-t-xl">
             <div className="flex h-[40px] w-[40px] justify-center items-center">
-              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="white" viewBox="0 0 256 256"><path d="M140,180a12,12,0,1,1-12-12A12,12,0,0,1,140,180ZM128,72c-22.06,0-40,16.15-40,36v4a8,8,0,0,0,16,0v-4c0-11,10.77-20,24-20s24,9,24,20-10.77,20-24,20a8,8,0,0,0-8,8v8a8,8,0,0,0,16,0v-.72c18.24-3.35,32-17.9,32-35.28C168,88.15,150.06,72,128,72Zm104,56A104,104,0,1,1,128,24,104.11,104.11,0,0,1,232,128Zm-16,0a88,88,0,1,0-88,88A88.1,88.1,0,0,0,216,128Z"></path></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" viewBox="0 0 256 256"><path d="M140,180a12,12,0,1,1-12-12A12,12,0,0,1,140,180ZM128,72c-22.06,0-40,16.15-40,36v4a8,8,0,0,0,16,0v-4c0-11,10.77-20,24-20s24,9,24,20-10.77,20-24,20a8,8,0,0,0-8,8v8a8,8,0,0,0,16,0v-.72c18.24-3.35,32-17.9,32-35.28C168,88.15,150.06,72,128,72Zm104,56A104,104,0,1,1,128,24,104.11,104.11,0,0,1,232,128Zm-16,0a88,88,0,1,0-88,88A88.1,88.1,0,0,0,216,128Z"></path></svg>
             </div>
-            <div className={`${variant2Data.size === "small" ? "w-[240px]" : "w-[422px]"} bg-[#3a86fd] text-white flex items-center h-[40px] px-4 py-2 text-[21px] rounded-tr-xl line-clamp-1`} style={{boxSizing:"border-box"}}>
+            <div className={`${variant2Data.size === "small" ? "w-[240px]" : "w-[422px]"} bg-[#3a86fd] text-white flex items-center h-[40px] px-4 py-2 text-[16px] rounded-tr-xl line-clamp-1`} style={{boxSizing:"border-box"}}>
               <span className="line-clamp-1">Question in {variant2Data.groupName}</span>
             </div>
           </div>
           <div className="flex bg-white border-b-[1px] border-[#cfd3de]">
             <div className="w-[50%] p-2 flex gap-x-2 items-center line-clamp-1 border-r-[1px] border-[#cfd3de]">
               <img src={variant2Data.img} className="w-[24px] h-[24px] rounded-full" alt="" />
-              <span className="line-clamp-1 text-lg font-[500] mt-[3px]">{variant2Data.username}</span>
+              <span className="line-clamp-1 text-lg font-[500] mt-[3px] text-[14px]">{variant2Data.username}</span>
             </div>
-            <div className="w-[50%] p-2 text-base flex items-center text-center">
+            <div className="w-[50%] p-2 text-base flex items-center text-center text-[12px]">
               {variant2Data.date}
             </div>
           </div>
-          <div className="px-4 py-2 text-lg rounded-b-xl bg-white">
+          <div className="px-4 py-2 text-lg rounded-b-xl bg-white text-[14px]">
             {variant2Data.question}
           </div>
         </div>
@@ -81,23 +81,23 @@ const NotificationCard: React.FC<NotificationCardProps> = ({variant, variant1Dat
         <div className={`${variant3Data.size==="small"?"w-[280px]":"w-[462px]"} flex flex-col`}>
           <div className="h-[40px] flex bg-[#64a1ff] rounded-t-xl">
             <div className="flex h-[40px] w-[40px] justify-center items-center">
-              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="white" viewBox="0 0 256 256"><path d="M213.66,82.34l-56-56A8,8,0,0,0,152,24H56A16,16,0,0,0,40,40V216a16,16,0,0,0,16,16H200a16,16,0,0,0,16-16V88A8,8,0,0,0,213.66,82.34ZM160,51.31,188.69,80H160ZM200,216H56V40h88V88a8,8,0,0,0,8,8h48V216Zm-42.34-77.66a8,8,0,0,1-11.32,11.32L136,139.31V184a8,8,0,0,1-16,0V139.31l-10.34,10.35a8,8,0,0,1-11.32-11.32l24-24a8,8,0,0,1,11.32,0Z"></path></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" viewBox="0 0 256 256"><path d="M213.66,82.34l-56-56A8,8,0,0,0,152,24H56A16,16,0,0,0,40,40V216a16,16,0,0,0,16,16H200a16,16,0,0,0,16-16V88A8,8,0,0,0,213.66,82.34ZM160,51.31,188.69,80H160ZM200,216H56V40h88V88a8,8,0,0,0,8,8h48V216Zm-42.34-77.66a8,8,0,0,1-11.32,11.32L136,139.31V184a8,8,0,0,1-16,0V139.31l-10.34,10.35a8,8,0,0,1-11.32-11.32l24-24a8,8,0,0,1,11.32,0Z"></path></svg>
             </div>
-            <div className={`${variant3Data.size === "small" ? "w-[240px]" : "w-[422px]"} bg-[#3a86fd] text-white flex items-center h-[40px] px-4 py-2 text-[21px] rounded-tr-xl line-clamp-1`} style={{boxSizing:"border-box"}}>
+            <div className={`${variant3Data.size === "small" ? "w-[240px]" : "w-[422px]"} bg-[#3a86fd] text-white flex items-center h-[40px] px-4 py-2 text-[16px] rounded-tr-xl line-clamp-1`} style={{boxSizing:"border-box"}}>
               <span className="line-clamp-1">Material in {variant3Data.groupName}</span>
             </div>
           </div>
           <div className="flex bg-white border-b-[1px] border-[#cfd3de]">
             <div className="w-[50%] p-2 flex gap-x-2 items-center line-clamp-1 border-r-[1px] border-[#cfd3de]">
               <img src={variant3Data.img} className="w-[24px] h-[24px] rounded-full" alt="" />
-              <span className="line-clamp-1 text-lg font-[500] mt-[3px]">{variant3Data.username}</span>
+              <span className="line-clamp-1 text-lg font-[500] mt-[3px] text-[14px]">{variant3Data.username}</span>
             </div>
-            <div className="w-[50%] p-2 text-base flex items-center text-center">
+            <div className="w-[50%] p-2 text-base flex items-center text-center text-[12px]">
               {variant3Data.date}
             </div>
           </div>
           <div className="px-4 py-2 text-lg rounded-b-xl flex items-center gap-x-4 bg-white justify-between">
-            <span className="line-clamp-2">{variant3Data.assignment}</span>
+            <span className="line-clamp-2 text-[14px]">{variant3Data.assignment}</span>
             <button onClick={variant3Data.onClick}>
               <svg width="57" height="36" viewBox="0 0 57 36" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <rect x="0.5" y="0.5" width="56" height="35" rx="3.5" stroke="#D0D5DD"/>
