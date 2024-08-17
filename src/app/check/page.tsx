09418dceb8ec3c7601dd3abef03f01c2bd95785e
Mainak Mukherjee <134730030+career-tokens@users.noreply.github.com>
2024-08-17 14:07:21 +0530

    Issue494 has been solved (#502)
    * doing the notifcation widget
    
    * notifcation card made
    
    * made some changes
    
    * some minor issue in example file
    
    * Clean up check file and adjust font sizes
    
    * Improve code formatting
    
    ---------
    
    Co-authored-by: mainak <heellomeelo@gmail.com>
    Co-authored-by: Jacob Heldt <jacobemil.heldt@gmail.com>

--- src/app/check/page.tsx
+++ src/app/check/page.tsx
@@ -1,32 +1,7 @@
-'use client'
-import Dropdown from '@/components/widgets/dropdown/Dropdown'
-import React, { useState } from 'react'
+import React from 'react'
 
 const Check: React.FC = () => {
-  const [opened, setOpened] = useState<boolean>(false)
-  const [currentChoice, setCurrentChoice] = useState<string>('Maths')
-  const choices: string[] = [
-    'English',
-    'Math',
-    'Science',
-    'Social Science',
-    'Computer Science',
-    'Geography',
-    'Chemistry',
-    'Physics'
-  ]
-
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <Dropdown
-        opened={opened}
-        setOpened={setOpened}
-        currentChoice={currentChoice}
-        setCurrentChoice={setCurrentChoice}
-        choices={choices}
-      />
-    </div>
-  )
+  return <div></div>
 }
 
 export default Check
