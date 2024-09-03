66588dca55b2ddc2a0387a58f78bede7f62de591
mainak <heellomeelo@gmail.com>
2024-09-03 08:58:37 +0530

    issue459 solved

---
+++ src/app/check/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import PageHeader from '@/components/layout/PageHeader'
+import React  from 'react'
+
+const Check: React.FC = () => {
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <PageHeader/>
+    </div>
+  )
+}
+
+export default Check
