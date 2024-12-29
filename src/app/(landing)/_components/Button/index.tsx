4c84ed9ebd75a18f846c2dba12be479238da6139
Jacob Heldt <jacobemil.heldt@gmail.com>
2024-12-29 14:07:01 +0100

    Improve code structure

--- src/app/(landing)/_components/Button/index.tsx
+++ src/app/(landing)/_components/Button/index.tsx
@@ -1,30 +1,30 @@
-import React from 'react';
-import { type ButtonProps } from './types';
+import React from 'react'
+import { type ButtonProps } from './types'
 
 const Button: React.FC<ButtonProps> = (props: ButtonProps): JSX.Element => {
-  const { onClick, variant = 'primary', size, children } = props;
+  const { onClick, variant = 'primary', size, children } = props
 
   // Define Tailwind classes based on props
   const baseClassName =
-    'rounded-md font-semibold transition-all duration-200 ease-in-out';
+    'rounded-md font-semibold transition-all duration-200 ease-in-out'
   const variantClassName =
     variant === 'outline'
       ? 'bg-transparent text-primary-500 border-2 border-primary-500 hover:bg-primary-100'
-      : 'bg-primary-500 text-white hover:bg-primary-700';
+      : 'bg-primary-500 text-white hover:bg-primary-700'
   const sizeClassName =
     size === 'big'
       ? 'text-lg py-2 px-5'
       : size === 'small'
-      ? 'text-sm py-2 px-4'
-      : 'text-base py-2.5 px-5';
+        ? 'text-sm py-2 px-4'
+        : 'text-base py-2.5 px-5'
 
-  const className = `${baseClassName} ${variantClassName} ${sizeClassName}`;
+  const className = `${baseClassName} ${variantClassName} ${sizeClassName}`
 
   return (
     <button className={className} onClick={onClick}>
       {children}
     </button>
-  );
-};
+  )
+}
 
-export default Button;
+export default Button
--- src/app/(landing)/_components/Hero/index.tsx
+++ src/app/(landing)/_components/Hero/index.tsx
@@ -1,46 +1,46 @@
-'use client';
+'use client'
 
-import { FaChevronDown, FaChevronRight } from 'react-icons/fa';
-import React, { useState, useEffect } from 'react';
-import Button from '@/app/(landing)/_components/Button';
+import { FaChevronDown, FaChevronRight } from 'react-icons/fa'
+import React, { useState, useEffect } from 'react'
+import Button from '@/app/(landing)/_components/Button'
 
-const words = ['Collaborative', 'Accessible', 'Engaging'];
+const words = ['Collaborative', 'Accessible', 'Engaging']
 
 const Hero = (): JSX.Element => {
   const handleLearnMoreClick = (): void => {
     // Smooth scroll to the "Learn More" section
-    const learnMoreSection = document.getElementById('mission');
+    const learnMoreSection = document.getElementById('mission')
     if (learnMoreSection) {
-      learnMoreSection.scrollIntoView({ behavior: 'smooth' });
+      learnMoreSection.scrollIntoView({ behavior: 'smooth' })
     }
-  };
+  }
 
   const handleViewOnGitHubClick = (): void => {
     // Open the GitHub page in a new tab
-    window.open('https://github.com/StudyCrew/StudyCrew', '_blank');
-  };
+    window.open('https://github.com/StudyCrew/StudyCrew', '_blank')
+  }
 
-  const [currentWord, setCurrentWord] = useState('Collaborative');
-  const [opacity, setOpacity] = useState(1);
+  const [currentWord, setCurrentWord] = useState('Collaborative')
+  const [opacity, setOpacity] = useState(1)
 
   useEffect(() => {
     const changeWord = (): void => {
       setCurrentWord((prevWord) => {
-        const nextIndex = (words.indexOf(prevWord) + 1) % words.length;
-        return words[nextIndex];
-      });
-      setOpacity(1);
-    };
+        const nextIndex = (words.indexOf(prevWord) + 1) % words.length
+        return words[nextIndex]
+      })
+      setOpacity(1)
+    }
 
     const interval = setInterval(() => {
-      setOpacity(0);
-      setTimeout(changeWord, 200);
-    }, 3500);
+      setOpacity(0)
+      setTimeout(changeWord, 200)
+    }, 3500)
 
     return () => {
-      clearInterval(interval);
-    };
-  }, []);
+      clearInterval(interval)
+    }
+  }, [])
 
   return (
     <div className="mx-16 mt-44 mb-16 text-center">
@@ -70,7 +70,7 @@ const Hero = (): JSX.Element => {
         </Button>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
