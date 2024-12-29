48c6249463093950acff0008df546377f313c3b9
Jacob Heldt <jacobemil.heldt@gmail.com>
2024-12-29 14:06:32 +0100

    Implement buttons

--- src/app/(landing)/_components/Hero/index.tsx
+++ src/app/(landing)/_components/Hero/index.tsx
@@ -1,37 +1,46 @@
-'use client'
+'use client';
 
-import { FaChevronDown } from 'react-icons/fa'
-import React, { useState, useEffect } from 'react'
-import Button from '@/app/(landing)/_components/Button'
+import { FaChevronDown, FaChevronRight } from 'react-icons/fa';
+import React, { useState, useEffect } from 'react';
+import Button from '@/app/(landing)/_components/Button';
 
-const words = ['Collaborative', 'Accessible', 'Engaging']
+const words = ['Collaborative', 'Accessible', 'Engaging'];
 
 const Hero = (): JSX.Element => {
   const handleLearnMoreClick = (): void => {
-    console.log('Learn More Clicked')
-  }
+    // Smooth scroll to the "Learn More" section
+    const learnMoreSection = document.getElementById('mission');
+    if (learnMoreSection) {
+      learnMoreSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
 
-  const [currentWord, setCurrentWord] = useState('Collaborative')
-  const [opacity, setOpacity] = useState(1)
+  const handleViewOnGitHubClick = (): void => {
+    // Open the GitHub page in a new tab
+    window.open('https://github.com/StudyCrew/StudyCrew', '_blank');
+  };
+
+  const [currentWord, setCurrentWord] = useState('Collaborative');
+  const [opacity, setOpacity] = useState(1);
 
   useEffect(() => {
     const changeWord = (): void => {
       setCurrentWord((prevWord) => {
-        const nextIndex = (words.indexOf(prevWord) + 1) % words.length
-        return words[nextIndex]
-      })
-      setOpacity(1)
-    }
+        const nextIndex = (words.indexOf(prevWord) + 1) % words.length;
+        return words[nextIndex];
+      });
+      setOpacity(1);
+    };
 
     const interval = setInterval(() => {
-      setOpacity(0)
-      setTimeout(changeWord, 200)
-    }, 3500)
+      setOpacity(0);
+      setTimeout(changeWord, 200);
+    }, 3500);
 
     return () => {
-      clearInterval(interval)
-    }
-  }, [])
+      clearInterval(interval);
+    };
+  }, []);
 
   return (
     <div className="mx-16 mt-44 mb-16 text-center">
@@ -45,22 +54,23 @@ const Hero = (): JSX.Element => {
         </span>
       </h1>
 
-      <p className="mt-3 text-xl font-normal text-secondary-text-700">
+      <p className="mt-4 text-xl font-normal text-secondary-text-700">
         Breaking barriers to education through the power of open source.
       </p>
 
-      <div className="mt-8 flex flex-col md:flex-row gap-2 justify-center align-middle">
-        <Button
-          onClick={handleLearnMoreClick}
-          variant="outline"
-          size="big-outline"
-        >
+      <div className="mt-12 flex flex-row gap-5 justify-center align-middle">
+        <Button onClick={handleLearnMoreClick} variant="primary" size="big">
           Learn More
           <FaChevronDown className="inline ml-2" />
         </Button>
+
+        <Button onClick={handleViewOnGitHubClick} variant="outline" size="big">
+          View on GitHub
+          <FaChevronRight className="inline ml-2" />
+        </Button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
