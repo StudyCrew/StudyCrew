bf907695cada442ae92d7217866abe25cca7dd82
Jacob <105601287+JacobHeldt@users.noreply.github.com>
2024-12-29 19:38:55 +0100

    Landing page redesign mission (#650)
    * Fix hero spacing problem
    
    * Move Logo Graphics to a seperate file
    
    * Update logos
    
    * Convert CSS to Tailwind
    
    * Adjust mission arrows desktop design
    
    * Adjust mission arrows
    
    * Fix overall desktop layout and content
    
    * Style fonts
    
    * Finish desktop design
    
    * Final touches
    
    * Linting

--- src/app/(landing)/_components/Mission/index.tsx
+++ src/app/(landing)/_components/Mission/index.tsx
@@ -1,126 +1,42 @@
 import React from 'react'
 import Image from 'next/image'
 
+import {
+  ChevronGradientSVG,
+  ChevronWhiteSVG,
+  SecondChevronGradientSVG
+} from './MissionArrows'
 import LOGO_GROUP_SVG from 'public/assets/mission/LogoGroup.svg' assert { type: 'svg' }
-
-import './style.css'
 
 const Mission = (props: any): JSX.Element => {
   return (
     <div>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 2628 376"
-        fill="none"
-        style={{ width: '100%' }}
-        className="chevron-gradient"
-        {...props}
-        preserveAspectRatio="none"
-      >
-        <path
-          fill="url(#a)"
-          d="m0 0 1297.2 264.759c8.37 1.708 12.56 2.563 16.8 2.563s8.43-.855 16.8-2.563L2628
-          0v105.002L1330.88 370.545c-8.41 1.722-12.62 2.582-16.88 2.581-4.26-.001-8.47-.865-16.87-2.592L0
-          104.085V0Z"
-        />
-        <defs>
-          <linearGradient
-            id="a"
-            x1={2628}
-            x2={-13.254}
-            y1={0}
-            y2={169.679}
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stopColor="stop-first" />
-            <stop offset={1} stopColor="#stop-second" />
-          </linearGradient>
-        </defs>
-      </svg>
-
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 2628 376"
-        fill="none"
-        style={{ width: '100%' }}
-        className="chevron-white"
-        {...props}
-        preserveAspectRatio="none"
-      >
-        <path
-          fill="url(#b)"
-          d="m0 0 1297.2 264.759c8.37 1.708 12.56 2.563 16.8 2.563s8.43-.855 16.8-2.563L2628
-          0v105.002L1330.88 370.545c-8.41 1.722-12.62 2.582-16.88 2.581-4.26-.001-8.47-.865-16.87-2.592L0
-          104.085V0Z"
-        />
-        <defs>
-          <linearGradient
-            id="b"
-            x1={2628}
-            x2={-13.254}
-            y1={0}
-            y2={169.679}
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stopColor="#FFFFFF" />
-            <stop offset={1} stopColor="#FFFFFF" />
-          </linearGradient>
-        </defs>
-      </svg>
-
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 2628 376"
-        fill="none"
-        style={{ width: '100%' }}
-        className="second-chevron-gradient"
-        {...props}
-        preserveAspectRatio="none"
-      >
-        <path
-          fill="url(#a)"
-          d="m0 0 1297.2 264.759c8.37 1.708 12.56 2.563 16.8 2.563s8.43-.855 16.8-2.563L2628
-          0v105.002L1330.88 370.545c-8.41 1.722-12.62 2.582-16.88 2.581-4.26-.001-8.47-.865-16.87-2.592L0
-          104.085V0Z"
-        />
-        <defs>
-          <linearGradient
-            id="a"
-            x1={2628}
-            x2={-13.254}
-            y1={0}
-            y2={169.679}
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stopColor="stop-first" />
-            <stop offset={1} stopColor="#stop-second" />
-          </linearGradient>
-        </defs>
-      </svg>
-
-      <div className="bg-gradient-to-r from-primary-500 to-gradient-500 h-full relative flex flex-col justify-center z-1 top-36">
-        <div className="flex flex-col md:flex-row items-center mt-32 gap-8 px-32">
-          <div className="lg:w-1/2 flex justify-center md:justify-start mb-8">
-            <div className="text-2xl lg:mb-8 mr-8">
-              <h3 className="text-zircon-50">OUR MISSION</h3>
-              <h2 className="text-zircon-50 slogan mb-4 leading-12">
+      <ChevronGradientSVG {...props} />
+      <ChevronWhiteSVG {...props} />
+      <SecondChevronGradientSVG {...props} />
+      <div className="md:px-32 px-7 md:pb-20 pb-11 bg-gradient-to-r from-primary-500 to-gradient-500 h-full relative flex flex-col justify-center z-10 md:top-[125px] top-[110px]">
+        <div className="md:grid md:grid-cols-2 md:gap-16 md:mt-32 mt-24 align-left">
+          <div className="flex justify-center md:justify-start mb-8">
+            <div>
+              <h3 className="text-zircon-50 md:text-xl text-lg font-semibold">
+                OUR MISSION
+              </h3>
+              <h2 className="text-white md:mb-4 mb-3 md:text-5xl text-4xl leading-none md:mt-0 mt-[-10px]">
                 We support education for all.
               </h2>
-              <p className="text-white">
-                StudyCrew is committed to making education accessible,
-                collaborative, and engaging through innovative learning
-                platforms. Our vision is to provide every student with the
-                resources and community they need to reach their full potential.
+              <p className="text-zircon-50 md:text-lg text-base">
+                We are dedicated to making education more accessible,
+                collaborative, and engaging through the development of an
+                open-source online learning platform. This platform will support
+                students on their learning journey and empower them to explore
+                their passions together. Our vision is to provide every student
+                with the resources and community they need to achieve their full
+                potential.
               </p>
             </div>
           </div>
-          <div className="md:flex md:justify-end md:my-8 hidden">
-            <Image
-              width={600}
-              height={600}
-              alt="Mission Logos"
-              src={LOGO_GROUP_SVG as string}
-            />
+          <div className="md:flex hidden justify-end">
+            <Image alt="Mission Logos" src={LOGO_GROUP_SVG as string} />
           </div>
         </div>
       </div>
