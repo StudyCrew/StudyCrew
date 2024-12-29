e6c34677b3c8b24b38965d4a6f69707c97fee02e
Jacob Heldt <jacobemil.heldt@gmail.com>
2024-12-29 18:21:22 +0100

    Style fonts

--- src/app/(landing)/_components/Mission/index.tsx
+++ src/app/(landing)/_components/Mission/index.tsx
@@ -17,13 +17,15 @@ const Mission = (props: any): JSX.Element => {
       <div className="bg-gradient-to-r from-primary-500 to-gradient-500 h-full relative flex flex-col justify-center z-10 md:top-[125px] top-[110px]">
         <div className="mx-32 grid grid-cols-2 mt-32 gap-16 align-center">
           <div className="flex justify-center md:justify-start mb-8">
-            <div className="text-2xl lg:mb-8 mr-8">
-              <h3 className="text-zircon-50">OUR MISSION</h3>
-              <h2 className="text-zircon-50 slogan mb-4 leading-12">
+            <div>
+              <h3 className="text-zircon-50 text-xl font-semibold">OUR MISSION</h3>
+              <h2 className="text-white mb-4 leading-none">
                 We support education for all.
               </h2>
               <p className="text-white">
-                We are dedicated to making education more accessible, collaborative, and engaging through the development of an open-source online learning platform. This platform will support students on their learning journey and empower them to explore their passions together. Our vision is to provide every student with the resources and community they need to achieve their full potential.
+                We are dedicated to making education more accessible,
+                collaborative, and engaging through the development of an
+                open-source online learning platform. This platform will support students on their learning journey and empower them to exploretheir passions together. Our vision is to provide every student with the resources and community they need to achieve their full potential.
               </p>
             </div>
           </div>
