821a26ab54ade76dd4403955bdd248800ae5f2ab
Jacob Heldt <jacobemil.heldt@gmail.com>
2024-12-31 20:00:28 +0100

    Make development section mobile responsive

--- src/app/(landing)/_components/Development/DevelopmentCard.tsx
+++ src/app/(landing)/_components/Development/DevelopmentCard.tsx
@@ -4,23 +4,25 @@ import { DevelopmentCardProps } from './types'
 const DevelopmentCard: React.FC<DevelopmentCardProps> = (
   props: DevelopmentCardProps
 ): JSX.Element => {
-  const { title, icon: Icon, children, last=false } = props
+  const { title, icon: Icon, children, last = false } = props
   return (
     <div>
-        <div className="bg-primary-100 py-4 px-6 rounded-lg flex items-center gap-4 text-primary-950">
-        <div className="text-3xl">{Icon && <Icon weight="duotone" />}</div>
+      <div className="bg-primary-100 py-4 px-6 rounded-lg flex items-center gap-4 text-primary-950">
+        <div className="text-3xl md:flex hidden">{Icon && <Icon weight="duotone" />}</div>
         <div>
-            <div className="text-xl font-medium">{title}</div>
-            <div className="md:text-lg text-base text-secondary-text-800 leading-snug">{children}</div>
+          <div className="text-xl font-medium flex gap-2"><span className='text-2xl md:hidden visible'>{Icon && <Icon weight="duotone" />}</span> {title}</div>
+          <div className="md:text-lg text-base text-secondary-text-800 leading-snug">
+            {children}
+          </div>
         </div>
-        </div>
+      </div>
 
-        {/* Line Connecting Cards */}
-        {!last && (
+      {/* Line Connecting Cards */}
+      {!last && (
         <div className="w-full flex justify-center">
           <div className="h-5 w-1 bg-primary-100"></div>
         </div>
-        )}
+      )}
     </div>
   )
 }
